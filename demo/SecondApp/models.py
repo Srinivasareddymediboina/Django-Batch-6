18ebf449c288277544c6ef9247447ffbec01d1f6
Girisha Devara <girisha.devara@gmail.com>
2020-09-22 17:49:10 +0530

    22-09-2020

---
+++ demo/SecondApp/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Register(models.Model):
+    name = models.CharField(max_length=50)
+    Email = models.EmailField(null=True, blank=True)
+    age = models.IntegerField()
+    phone = models.CharField(null=True, blank=True, max_length=10)
+
+    def __str__(self):
+        return self.name + str(self.age)
