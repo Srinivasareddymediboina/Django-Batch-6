bfce8abacd6a0a7c57559a913f4a5d2ac083459e
Anil Polineni <anilpolineni09@gmail.com>
2020-10-03 07:07:17 +0530

    Update settings.py
    Change

--- Day-17/mailsending/settings.py
+++ Day-17/mailsending/settings.py
@@ -152,5 +152,5 @@ STATIC_URL = '/static/'
 EMAIL_USE_TLS=True
 EMAIL_HOST="smtp.gmail.com"
 EMAIL_PORT=587
-EMAIL_HOST_USER="anilpolineni008@gmail.com"
-EMAIL_HOST_PASSWORD="anil@1234"
+EMAIL_HOST_USER="××××××××"
+EMAIL_HOST_PASSWORD="××××××××"
